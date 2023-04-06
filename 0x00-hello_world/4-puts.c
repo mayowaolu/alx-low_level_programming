6b5dea3f800fedb2ce7aeba83e1bf4276b608a2f
John Olusetire <johnolusetire97@gmail.com>
2023-04-06 13:34:39 +0000

    Practice

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
- **/
+ * main - entry point
+ *
+ * Return: (0) success
+ */
+
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
+
 	return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Return: 0
+ * main - entry point
+ *
+ * Return: (0) on success
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
