0104654b9fe56b3e866ff6f5e0e6a134fd91c5f1
John Olusetire <johnolusetire97@gmail.com>
2023-04-13 17:06:23 +0000

    Adds solution to task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,22 +7,31 @@
 
 int main(void)
 {
-	int i = 48;
-	int j = 48;
-	int m = 48;
-	int n = 49;
+	int n1 = 0;
+	int n2 = 0;
 
-	while (i < 58)
+	while (n1 < 99)
 	{
-		while (j < 58)
+		while (n2 < 100)
 		{
-			while (m < 58)
-			{
-				while (n < 58)
-				{
+			putchar((n1 / 10) + '0');
+			putchar((n1 % 10) + '0');
+			putchar(' ');
+			putchar((n2 / 10) + '0');
+			putchar((n2 % 10) + '0');
 
-				}
+			if (n1 + n2 < 197)
+			{
+				putchar(',');
+				putchar(' ');
 			}
+			n2++;
 		}
+		n1++;
+		n2 = n1 + 1;
 	}
+
+	putchar('\n');
+
+	return (0);
 }
