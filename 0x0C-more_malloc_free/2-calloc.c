bc76a7bc930b578d4be89d5c9d997ee3d9a0e6e6
John Olusetire <johnolusetire97@gmail.com>
2023-04-15 00:06:58 +0000

    Corrects solutions

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,15 +13,16 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *p;
 	unsigned int i;
 
-	if (nmemb <= 0 || size <= 0)
+	if (nmemb == 0 || size == 0)
 		return (NULL);
 	if (size >= UINT_MAX / nmemb || nmemb >= UINT_MAX / size)
 		return (NULL);
+
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
 		p[i] = 0;
 
-	return ((void *)p);
+	return (p);
 }
