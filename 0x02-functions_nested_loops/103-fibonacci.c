23527451912b978f8e977437276d60c660617418
John Olusetire <johnolusetire97@gmail.com>
2023-04-18 23:42:00 +0000

    Adds solution

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,1 +1,31 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * main - entry block
+ * Description: prints the first 50 fibonacci numbers
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	long n1, n2;
+	long tmp = 0;
+	long sum;
+
+	n1 = 1;
+	n2 = 2;
+	sum = 2;
+
+	while (tmp < 4000000)
+	{
+		tmp = n1 + n2;
+		if (tmp % 2 == 0)
+			sum = sum + tmp;
+
+		n1 = n2;
+		n2 = tmp;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,1 +1,33 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * main - entry block
+ * Description: prints the first 50 fibonacci numbers
+ *
+ * Return: (0)
+ */
+
+int main(void)
+{
+	long n1, n2;
+	int m = 0;
+	long tmp;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%lu, ", n1);
+	printf("%lu, ", n2);
+
+	while (m < 96)
+	{
+		tmp = n1 + n2;
+		if (m < 95)
+			printf("%lu, ", tmp);
+		else
+			printf("%lu\n", tmp);
+		n1 = n2;
+		n2 = tmp;
+		m++;
+	}
+	return (0);
+}
