173396652f478c96fc43012fee7f6e7b6d5d7e76
John Olusetire <johnolusetire97@gmail.com>
2023-04-21 23:19:54 +0000

    Corrects header doc in0_sum...c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,11 +1,11 @@
 #include "variadic_functions.h"
-
 /**
  * sum_them_all - return the sum of all its parameters
  * @n: number of extra parameters
  *
  * Return: total sum
  */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
