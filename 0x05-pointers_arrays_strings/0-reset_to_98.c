62a7a12a8ea9570022bcb82ce54dc0043b46cd41
John Olusetire <johnolusetire97@gmail.com>
2023-04-21 01:24:54 +0000

    Adds solution

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - updates value to 98
- * @*n: pointer
+ * @n: pointer
  */
 
 void reset_to_98(int *n)
--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,1 +1,15 @@
+#include "main.h"
+/**
+ * swap_int - swaps two integer values
+ * @a: integer value
+ * @b: integer value
+ */
 
+void swap_int(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
