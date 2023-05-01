119d4108eaf7ec8ed790184260c3db20eeb2ec19
John Olusetire <johnolusetire97@gmail.com>
2023-05-01 16:09:49 +0000

    Adds solution to task 12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,1 +1,44 @@
 #include "main.h"
+#include <stdio.h>
+/**
+ * print_buffer - prints a buffer
+ * @b: pointer to string
+ * @size: size of string
+ */
+
+void print_buffer(char *b, int size)
+{
+	int i, j, k;
+	char temp[11];
+
+	i = 0;
+	if (size == 0)
+		putchar('\n');
+	while (i < size)
+	{
+		printf(i % 10 == 0 ? "%08x: " : "", i);
+		j = 0;
+		while (j < 10 && i + j < size)
+		{
+			printf(j % 2 == 1 ? "%02x " : "%02x", b[i + j]);
+			temp[j] = b[i + j];
+			j++;
+		}
+		while (j < 10)
+		{
+			printf(j % 2 == 1 ? "   " : "  ");
+			j++;
+		}
+		k = 0;
+		while (k < 10 && i + k < size)
+		{
+			if (temp[k] < 32)
+				printf(".");
+			else
+				printf("%c", temp[k]);
+			k++;
+		}
+		putchar('\n');
+		i += j;
+	}
+}
